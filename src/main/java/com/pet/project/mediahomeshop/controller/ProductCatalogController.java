36e6ab64d7a452bebc90e0e2abd4ea987859480d
zodakuryakova <zodaturaeva@gmail.com>
2025-11-06 21:17:16 +0100

    DEV-2: some improwments

--- src/main/java/com/pet/project/mediahomeshop/controller/ProductCatalogController.java
+++ src/main/java/com/pet/project/mediahomeshop/controller/ProductCatalogController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-// instead autowired
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/products")
--- src/main/java/com/pet/project/mediahomeshop/entity/Product.java
+++ src/main/java/com/pet/project/mediahomeshop/entity/Product.java
@@ -1,7 +1,14 @@
 package com.pet.project.mediahomeshop.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name="products")
 public class Product {
@@ -21,58 +28,6 @@ public class Product {
     @Column(name="imageurl")
     private String imageUrl;
 
+    @Column(name ="category_id")
     private int category_id;
-
-    public Product() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                ", category_id=" + category_id +
-                '}';
-    }
 }
